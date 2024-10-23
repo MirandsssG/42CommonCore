b8679a08d9d50910776a34bb6f4eb885860e1e68
Diogo VM <diogo.miranda.tmo16@gmail.com>
2024-10-23 01:13:14 +0100

    added exs

--- PiscineReloaded/ex21/ft_range.c
+++ PiscineReloaded/ex21/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:13:36 by mirandsssg        #+#    #+#             */
-/*   Updated: 2024/10/22 18:17:25 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2024/10/22 23:57:37 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,41 @@
 
 int *ft_range(int min, int max)
 {
-	
-} 
+	int	range;
+	int	i;
+	int	*buffer;
+
+	if (min >= max)
+		return (0);
+	range = max - min;
+	buffer = malloc(range * sizeof(int));
+	if (!buffer)
+		return (0);
+	i = 0;
+	while (i < range)
+	{
+		buffer[i] = min + i;
+		i++;
+	}
+	return (buffer);
+}
+
+int	main()
+{
+	int	min;
+	int	max;
+	int	*tab;
+	int	i = 0;
+	int	size;
+
+	min = -10;
+	max = 10;
+	size = max - min;
+	tab = ft_range(min, max);
+	while (i < size)
+	{
+		printf("%d\n", tab[i]);
+		i++;
+	}
+	return (0);
+}
