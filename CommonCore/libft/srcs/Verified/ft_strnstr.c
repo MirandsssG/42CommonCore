6e46f1f07459927b0a37de76c4f756c6398acc16
Diogo VM <diogo.miranda.tmo16@gmail.com>
2024-10-29 17:46:40 +0000

    added exs

--- CommonCore/libft/srcs/Verified/ft_strnstr.c
+++ CommonCore/libft/srcs/Verified/ft_strnstr.c
@@ -6,8 +6,28 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 03:16:25 by mirandsssg        #+#    #+#             */
-/*   Updated: 2024/10/27 03:29:45 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2024/10/29 17:36:03 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!*little)
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] && little[j] && i + j < len && big[i + j] == little[j])
+			j++;
+		if (!little[j])
+			return ((char *)(big + i));
+		i++;
+	}
+	return (NULL);
+}
