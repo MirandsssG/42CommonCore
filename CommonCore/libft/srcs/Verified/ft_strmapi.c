8ec719d8bef2f151a74cd80c55bd35ce37ece639
Diogo VM <diogo.miranda.tmo16@gmail.com>
2024-11-02 21:37:51 +0000

    added Exs

--- CommonCore/libft/srcs/Verified/ft_strmapi.c
+++ CommonCore/libft/srcs/Verified/ft_strmapi.c
@@ -6,8 +6,25 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 12:25:51 by mirandsssg        #+#    #+#             */
-/*   Updated: 2024/10/30 12:25:53 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2024/11/02 21:21:33 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	size_t	i;
+
+	if (!(str = ft_strdup(s)))
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		str[i] = (*f)(i, str[i]);
+		i++;
+	}
+	return (str);
+}
