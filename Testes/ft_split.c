81fa479eac187042462e3e1b04e31aa95b678d92
Diogo VM <diogo.miranda.tmo16@gmail.com>
2024-11-06 03:37:32 +0000

    added Exs

--- Testes/ft_split.c
+++ Testes/ft_split.c
@@ -6,14 +6,58 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 15:36:44 by mirandsssg        #+#    #+#             */
-/*   Updated: 2024/10/31 15:40:46 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2024/11/05 17:42:49 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-char **ft_split(char const *s, char c)
+static	char **ft_alloc_split(char const *s, char c)
+{
+	size_t	i;
+	char	**split;
+	size_t	total;
+	
+	i = 0;
+	total = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			total++;
+		i++;
+	}
+	split = (char**)malloc(sizeof(s) * (total + 2));
+	if (!split)
+		return (NULL);
+	return (split);
+}
+void	*ft_free_split(char **split, size_t count)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < count)
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	return (NULL);
+}
+
+static	void ft_split_by_char(char **split, char const *s, char c)
 {
 	
+}
+
+char **ft_split(char const *s, char c)
+{
+	char **split;
+
+	if (!(split == ft_alloc_split(s, c)));
+		return (NULL);
+	if (!ft_split_by_char(split, s, c));
+		return (NULL);
+	return (split);
 }
