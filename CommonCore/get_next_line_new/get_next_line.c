2426e9f30c5bd58df1d09aa2e4a631099c2b80a5
MirandsssG <diogo.miranda.tmo16@gmail.com>
2025-01-08 14:16:07 +0000

    added get_next_line_new

---
+++ CommonCore/get_next_line_new/get_next_line.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 13:41:39 by mirandsssg        #+#    #+#             */
+/*   Updated: 2025/01/08 14:15:30 by mirandsssg       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+    static char	buff[BUFFER_SIZE + 1];
+	ssize_t		bytes_read;
+	char		*line;
+
+    line = NULL;
+    bytes_read = 1;
+    while ((fd >= 0 && BUFFER_SIZE > 0) && bytes_read > 0)
+    {
+        if (buff[0])
+        {
+            line = ft_strjoin(line, buff);
+            if (!line)
+                return (NULL);
+            if (line[ft_strlen(line) - 1] == '\n')
+                break ;
+        }
+        else
+        {
+            bytes_read = read(fd, buff, BUFFER_SIZE);
+            if (bytes_read < 0)
+                return (free(line), NULL);
+            buff[bytes_read] = '\0';
+        }
+    }
+    return (line);
+}
+
+int main(void)
+{
+    int fd = open("test_file.txt", O_RDONLY);
+    if (fd < 0)
+    {
+        perror("Error opening file");
+        return (1);
+    }
+
+    char *line;
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("Line: %s", line);
+        free(line);
+    }
+
+    close(fd);
+    return (0);
+}
---
+++ CommonCore/get_next_line_new/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 13:41:25 by mirandsssg        #+#    #+#             */
+/*   Updated: 2025/01/08 13:41:28 by mirandsssg       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char    *get_next_line(int fd);
+size_t  ft_strlen(char *str);
+char    *ft_strjoin(char *s1, char *s2);
+
+#endif
