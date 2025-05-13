a7784a5a733a293e2853279cf78405300b841f3a
MirandsssG <diogo.miranda.tmo16@gmail.com>
2025-05-14 00:02:26 +0100

    added so_long

---
+++ CommonCore/so_long/main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 23:42:26 by mirandsssg        #+#    #+#             */
+/*   Updated: 2025/05/14 00:01:02 by mirandsssg       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int main (int ac, char **av)
+{
+    if (ac != 2)
+    {
+        ft_printf("Usage: %s <filename>\n", av[0]);
+        return (1);
+    }
+    t_data data;
+    data.rows = line_count(av[1])
+    if (data.rows <= 0)
+    {
+        ft_printf("Failed to read lines or empty file\n");
+        return (1);
+    }
+    data.mapa = malloc(sizeof(char *) * (data.rows + 1));
+    if (!data.mapa)
+    {
+        perror("malloc");
+        return (1);
+    }
+    int fd = open(av[1], O_RDONLY);
+    if (fd < 0)
+    {
+        perror("open");
+        free(data.mapa);
+        return (1);
+    }
+    int i;
+    
+    i = 0;
+    char *line;
+    while ((line = get_next_line(fd)));
+        data.mapa[i++] = line;
+    data.mapa[i] = NULL;
+    close(fd);
+}
--- CommonCore/so_long/so_long.h
+++ CommonCore/so_long/so_long.h
@@ -6,16 +6,25 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:08:26 by mirandsssg        #+#    #+#             */
-/*   Updated: 2025/05/13 20:17:07 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2025/05/13 23:41:21 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
+# include "/includes/get_next_line/get_next_line.h"
+# include "/includes/libftprintf/ft_printf.h"
+# include "/includes/libft/libft.h"
+# include "/includes/minilibx-linux/mlx.h"
+# include "/includes/minilibx-linux/mlx_int.h"
+
+typedef struct s_data
+{
+    int columns;
+    int rows;
+    char **mapa;
+}   t_data
 
 
 
