6d093c2333f90ee4dec0e51f610149394eb23fe0
MirandsssG <diogo.miranda.tmo16@gmail.com>
2025-05-16 00:15:59 +0100

    added so_long

---
+++ CommonCore/so_long/exit.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 23:48:17 by mirandsssg        #+#    #+#             */
+/*   Updated: 2025/05/15 23:57:42 by mirandsssg       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int ft_exit(t_data *data)
+{
+    if(data->img_collectible)
+        mlx_destroy_image(data->mlx_ptr, data->img_collectible);
+    if(data->img_exit)
+        mlx_destroy_image(data->mlx_ptr, data->img_exit);
+    if(data->img_floor)
+        mlx_destroy_image(data->mlx_ptr, data->img_floor);
+    if(data->img_player)
+        mlx_destroy_image(data->mlx_ptr, data->img_player);
+    if(data->img_wall)
+        mlx_destroy_image(data->mlx_ptr, data->img_wall);
+    if(data->mlx_ptr)
+        mlx_destroy_display(data->mlx_ptr);
+    if(data->mlx_ptr)
+        mlx_loop_end(data->mlx_ptr);
+    free(data->mlx_ptr);
+    free_map(data);
+    exit (1);
+}
--- CommonCore/so_long/so_long.h
+++ CommonCore/so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:08:26 by mirandsssg        #+#    #+#             */
-/*   Updated: 2025/05/15 22:57:36 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2025/05/16 00:15:27 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,16 @@
 # include "includes/libft/libft.h"
 # include "includes/minilibx-linux/mlx.h"
 # include "includes/minilibx-linux/mlx_int.h"
+
+# define ESC 65307
+# define W_KEY 119
+# define A_KEY	97
+# define S_KEY	115
+# define D_KEY	100
+# define UP_KEY	65362
+# define DOWN_KEY 65364
+# define RIGHT_KEY 65363
+# define LEFT_KEY 65361
 
 typedef struct s_data
 {
@@ -31,7 +41,10 @@ typedef struct s_data
     void *img_player;
     void *img_exit;
     void *img_collectible;
+    int moves;
 }   t_data;
+
+int ft_exit(t_data *data);
 
 // Initialize
 void    print_map(char **map);
@@ -60,5 +73,13 @@ int is_path_valid(t_data *data);
 int initialize_mlx(t_data *data);
 int    draw_map(t_data *data);
 int load_images(t_data *data);
+int key_handler(t_data *data);
+void    free_map(t_data *data);
+
+// MOVE
+void    move_up(t_data *data);
+void    move_left(t_data *data);
+void    move_right(t_data *data);
+void    move_down(t_data *data);
 
 #endif
