29b1cbd5e3bc3d4db092489e0f9104963a13b3e7
Diogo Luis Magalhaes Miranda <dluis-ma@c1r9s9.42lisboa.com>
2025-05-22 10:50:14 +0100

    added so_long

--- CommonCore/so_long/exit.c
+++ CommonCore/so_long/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
+/*   By: dluis-ma <dluis-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 23:48:17 by mirandsssg        #+#    #+#             */
-/*   Updated: 2025/05/17 22:18:16 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2025/05/22 10:46:48 by dluis-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ int ft_exit(t_data *data)
         mlx_destroy_image(data->mlx_ptr, data->img_player);
     if(data->img_wall)
         mlx_destroy_image(data->mlx_ptr, data->img_wall);
-    if(data->mlx_ptr)
-        mlx_destroy_display(data->mlx_ptr);
     if(data->win_ptr)
         mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+    if(data->mlx_ptr)
+        mlx_destroy_display(data->mlx_ptr);
     if(data->mlx_ptr)
         mlx_loop_end(data->mlx_ptr);
     free(data->mlx_ptr);
--- CommonCore/so_long/initialize/initialize.c
+++ CommonCore/so_long/initialize/initialize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   initialize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
+/*   By: dluis-ma <dluis-ma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 02:35:24 by mirandsssg        #+#    #+#             */
-/*   Updated: 2025/05/17 22:12:50 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2025/05/22 10:45:34 by dluis-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,44 +77,3 @@ int initialize_vars(char *map, t_data *data)
     // printf("\n\nValid map\n");
     return 0;
 }
-
-// int    initialize_vars(char *map)
-// {
-//     t_data *data;
-//     data.rows = line_count(map);
-//     if (data.rows <= 0)
-//     {
-//         printf("Failed to read lines or empty file\n");
-//         return (1);
-//     }
-//     data.map = malloc(sizeof(char *) * (data.rows + 1));
-//     if (!data.map)
-//     {
-//         perror("malloc");
-//         return (1);
-//     }
-//     int fd = open(map, O_RDONLY);
-//     if (fd < 0)
-//     {
-//         perror("open");
-//         free(data.map);
-//         return (1);
-//     }
-//     int i = 0;
-//     char *line;
-//     while ((line = get_next_line(fd)))
-//     {
-//         data.map[i++] = line;
-//     }
-//     data.map[i] = NULL;
-//     close(fd);
-//     data.columns = str_len_no_nl(data.map[0]);
-//     print_map(data.map);
-//     if (!parse_map(data.map, map, data))
-//     {
-//         printf("Invalid map\n");
-//         return (1);
-//     }
-//     printf("\n\nValid map\n");
-//     return 0;
-// }
