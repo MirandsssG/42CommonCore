6327db133f611136590fd3384b2e077e2b778549
MirandsssG <diogo.miranda.tmo16@gmail.com>
2025-06-18 14:12:09 +0100

    added minishell

---
+++ CommonCore/minishell/minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 12:56:59 by mirandsssg        #+#    #+#             */
+/*   Updated: 2025/06/18 13:00:44 by mirandsssg       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <errno.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <limits.h>
+# include <fcntl.h>
+
+typedef	struct s_data
+{
+	
+}	t_data;
+
+
+#endif
