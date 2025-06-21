3a284e76b665bd62467a3393a6c9ea6fad7d99a4
MirandsssG <diogo.miranda.tmo16@gmail.com>
2025-06-21 17:17:46 +0100

    added minishell

--- CommonCore/minishell/main.c
+++ CommonCore/minishell/main.c
@@ -6,11 +6,20 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 12:53:55 by mirandsssg        #+#    #+#             */
-/*   Updated: 2025/06/21 16:31:12 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2025/06/21 17:15:08 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void ctrlc_handler(int sig)
+{
+    (void)sig;
+    rl_replace_line("", 0);
+    write(1, "\n", 1);
+    rl_on_new_line();
+    rl_redisplay();
+}
 
 int	main(int ac, char **av)
 {
@@ -19,10 +28,17 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 	ft_memset(&data, 0, sizeof(t_data));
+	
+	signal(SIGINT, ctrlc_handler);
+	signal(SIGQUIT, SIG_IGN);
 	while (1)
 	{
 		data.input = readline("minishell$ ");
-		if (!data.input) break;
+		if (!data.input)
+		{
+			printf("\nexit\n");
+			break;
+		}
 		if (*data.input)
 			add_history(data.input);
 		free(data.input);
