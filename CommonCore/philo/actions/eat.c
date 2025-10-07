2b77d746acacbaefab2e35af0c8343b656d88827
MirandsssG <diogo.miranda.tmo16@gmail.com>
2025-10-07 23:55:20 +0100

    added philo

--- CommonCore/philo/actions/eat.c
+++ CommonCore/philo/actions/eat.c
@@ -6,7 +6,7 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 21:30:55 by mirandsssg        #+#    #+#             */
-/*   Updated: 2025/10/07 22:05:38 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2025/10/07 23:48:58 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	eat(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->data->lock_print);
 	philo->last_meal_time = get_time_ms();
+	philo->meals_eaten++;
 	pthread_mutex_unlock(&philo->data->lock_print);
 	print_status(philo, "is eating");
-	philo->meals_eaten++;
 	usleep(philo->data->time_to_eat * 1000);
 }
--- CommonCore/philo/execute/monitor_routine.c
+++ CommonCore/philo/execute/monitor_routine.c
@@ -6,7 +6,7 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 21:43:15 by mirandsssg        #+#    #+#             */
-/*   Updated: 2025/10/07 21:46:17 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2025/10/07 23:47:22 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ void	*monitor_routine(void *arg)
 {
 	t_data	*data = (t_data *)arg;
 	int	i;
+	int	all_full;
 
 	while (!data->dead)
 	{
 		i = 0;
+		all_full = 1;
 		while (i < data->number_of_philos)
 		{
 			if (get_time_ms() - data->philos[i].last_meal_time > data->time_to_die)
@@ -28,7 +30,14 @@ void	*monitor_routine(void *arg)
 				data->dead = 1;
 				return (NULL);
 			}
+			if (data->must_eat_count > 0 && data->philos[i].meals_eaten < data->must_eat_count)
+				all_full = 0;
 			i++;
+		}
+		if (data->must_eat_count > 0 && all_full)
+		{
+			data->dead = 1;
+			return (NULL);
 		}
 		usleep(1000);
 	}
