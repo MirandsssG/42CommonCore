4f72bc63b366ba65e4c1ccd9ef20ceffee12ac9b
MirandsssG <diogo.miranda.tmo16@gmail.com>
2025-11-23 21:35:43 +0000

    added exam03

--- exam03/filter/tries/filter.c
+++ exam03/filter/tries/filter.c
@@ -1,9 +1,9 @@
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#define BUFFER_SIZE 30
+#define BUFFER_SIZE 3
 
-int	ft_strlen(char *s)
+int		ft_strlen(char *s)
 {
 	int i = 0;
 	if (!s)
@@ -13,7 +13,7 @@ int	ft_strlen(char *s)
 	return (i);
 }
 
-int	ft_strncmp(char *s1, char *s2, int n)
+int		ft_strncmp(char *s1, char *s2, int n)
 {
 	int i = 0;
 	while (i < n && s1[i] && s2[i])
@@ -27,14 +27,54 @@ int	ft_strncmp(char *s1, char *s2, int n)
 	return (0);
 }
 
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	char 	*pattern = av[1];
-	int		pattern_len = ft_strlen(pattern);
+	char	*pattern = av[1];
+	int		pat_len = ft_strlen(pattern);
 	char	buffer[BUFFER_SIZE];
 	char	window[BUFFER_SIZE];
-	int		window_len = 0;
+	int		win_len = 0;
 	int		bytes;
 	int		i;
 	int		j;
+
+	if (ac != 2)
+		return (1);
+	 while ((bytes = read(0, buffer, BUFFER_SIZE)) > 0)
+	 {
+		i = 0;
+		while (i < bytes)
+		{
+			window[win_len++] = buffer[i];
+			if (win_len > pat_len)
+			{
+				write(1, &window[0], 1);
+				j = 0;
+				while (j < win_len)
+				{
+					window[j] = window[j + 1];
+					j++;
+				}
+				win_len--;
+			}
+			if (win_len == pat_len && ft_strncmp(window, pattern, pat_len) == 0)
+			{
+				j = 0;
+				while (j < pat_len)
+				{
+					write(1, "*", 1);
+					j++;
+				}
+				win_len = 0;
+			}
+			i++;
+		}
+	 }
+	 j = 0;
+	 while (j < win_len)
+	 {
+		write(1, &window[j], 1);
+		j++;
+	 }
+	 return (0);
 }
