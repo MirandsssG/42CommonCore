044eed64529c4d39bdf6eaa4657d504d9282eac0
MirandsssG <diogo.miranda.tmo16@gmail.com>
2026-02-26 00:45:16 +0000

    added philo

--- CommonCore/philo/main.c
+++ CommonCore/philo/main.c
@@ -6,7 +6,7 @@
 /*   By: mirandsssg <mirandsssg@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 18:57:54 by mirandsssg        #+#    #+#             */
-/*   Updated: 2026/02/18 14:18:15 by mirandsssg       ###   ########.fr       */
+/*   Updated: 2026/02/25 23:31:08 by mirandsssg       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,13 @@ int	main(int ac, char **av)
 	if (validate_args(ac, av))
 		return (1);
 	parse_args(ac, av, &data);
+	if (data.number_of_philos == 1)
+	{
+		printf("0 1 has taken a fork\n");
+		usleep(data.time_to_die * 1000);
+		printf("%d 1 died\n", data.time_to_die);
+		return (0);
+	}
 	if (init_data(&data))
 		return (cleanup_data(&data), 1);
 	if (execute(&data))
